e826280b436138a0a38fb2ea42ffba9a6d76cdb4
JPALX04 <184169107+JPALX04@users.noreply.github.com>
2025-05-28 22:17:44 -0300

    feat(ui): add color selection and erase functionality
    - Add rainbow color palette button
    - Add solid black color option
    - Implement erase button

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const mainContainer = document.querySelector("#mainContainer");
 const template = document.querySelector(".template");
 const blackBoard = document.querySelector(".blackBoard");
+const buttons = document.querySelector(".buttons");
 const rangeField = document.querySelector(".rangeField");
 const rangeTxt = document.querySelector(".rangeTxt");
 
@@ -38,11 +39,37 @@ function setRangeValue() {
   rangeField.addEventListener("input", inputEvent);
 }
 
+let gridColor;
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
 gridContainer.addEventListener("mouseover", (e) => {
   if (e.target.matches(".gridDiv")) {
-    e.target.style.backgroundColor = "black ";
-    // console.log(e.target, e.currentTarget);
+    if (gridColor === "rainbow") {
+      e.target.style.backgroundColor = getRandomColor();
+    }
+    e.target.style.backgroundColor = gridColor;
   }
 });
+
+btnClick = (e) => {
+  if (e.target.matches(".colors")) {
+    gridColor = "rgb(0,0,0)";
+  }
+  if (e.target.matches(".rainbow")) {
+    gridColor = "rainbow";
+  }
+  if (e.target.matches(".rubber")) {
+    gridContainer.innerHTML = "";
+    loadPage();
+  }
+};
+
+buttons.addEventListener("click", btnClick);
 
 loadPage();
